b33ec8e1f711ec6a083d3f5eada80017ddb72696
Jarkko Saikkonen <jarkko.saikkonen@gmail.com>
2018-07-03 16:43:17 +0300

    Cleaning and adding a menu

--- js/webcv.js
+++ js/webcv.js
@@ -5,13 +5,19 @@
 $(function () {
     "use strict";
     
-    var progressBarNamesArray = ["precision-bar", "systematic-bar", "fast-learning-bar", "music-bar", "video-bar", "front-end-bar"],
-        progressBarPercentageArray = ["90%", "94%", "95%", "94%", "65%", "55%"],
+    var progressBarNamesArray = ["precision-bar", "systematic-bar", "fast-learning-bar", "music-bar", "video-bar", "front-end-bar", "written-bar"],
+        progressBarPercentageArray = ["90%", "94%", "95%", "94%", "50%", "55%", "92%"],
         i;
     
-    for (i = 0; i < progressBarNamesArray.length; i + 1) {
+    for (i = 0; i < progressBarNamesArray.length; i += 1) {
         $("#" + progressBarNamesArray[i]).animate({
             width: progressBarPercentageArray[i]
         }, { duration: 300, queue: false });
     }
+	
+	
+	$(".nav-link").click(function () {
+		$(".nav-link").removeClass("active");
+		$(this).addClass("active");
+	});
 });
